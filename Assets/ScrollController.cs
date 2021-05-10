1e29c5a9c2fe9f8addc511705d7d2b7b021d6061
Suvamjyoti_Panda <likun54@outlook.com>
2021-05-10 09:04:37 +0530

    panel UI setup

--- Assets/ScrollController.cs
+++ Assets/ScrollController.cs
@@ -7,92 +7,56 @@ using DG.Tweening;
 
 public class ScrollController : MonoBehaviour
 {
-    [SerializeField]private int _startSerialNo;
+    [SerializeField] private int _startSerialNo;
 
-    private List<Button> _highScorePanelList;    
-    private float[] _distanceArray;
+    private panelView[] _highScorePanelList;
+    
+    //private float[] _distanceArray;
+    //private float _panelGap;
 
     [SerializeField] private Button moveUp;
     [SerializeField] private Button moveDown;
-
-    private int currentPanelNo;
-    private Vector3 currentPanelPosition;
-    private Vector3 topPanelPosition;
-    private Vector3 bottomPanelPosition;
-
-    [SerializeField] private GameObject currentPanel;
-    [SerializeField] private GameObject topPanel;
-    [SerializeField] private GameObject bottomPanel;
-//    [SerializeField] private GameObject newPanel;
 
 
     #region Initialisation
 
     private void Start()
     {
-        //Initialisation();
+        Initialisation();
+        
+        //set panel values
 
-        currentPanelPosition = currentPanel.transform.position;
-        topPanelPosition = topPanel.transform.position;
-        bottomPanelPosition = bottomPanel.transform.position;
-
-
-        //currentPanel.SetActive(true);
-        //topPanel.SetActive(false);
-        //bottomPanel.SetActive(false);
-        //newPanel.SetActive(false);
 
         moveUp.onClick.AddListener(MoveUp);
         moveDown.onClick.AddListener(MoveDown);
-
 
     }
 
     private void Initialisation()
     {
-        GameObject[] btn = GameObject.FindGameObjectsWithTag("button");
+        panelView[] _highScorePanelList = transform.GetComponentsInChildren<panelView>();
 
-        //get all the panels
-        foreach (GameObject gameObject in btn)
-        {
-            _highScorePanelList.Add(gameObject.GetComponent<Button>());
-        }
+        Debug.Log(_highScorePanelList.Length);
+
+        //for (int j = 0; j < _highScorePanelList.Length; j++)
+        //{
+        //    RectTransform rectTransform = _highScorePanelList[i].GetComponent<RectTransform>();
+        //    rectTransform.position = new Vector3(rectTransform.position.x, _distanceArray[i],rectTransform.position.z);
+        //}
+
     }
 
-    
+
     #endregion
 
     private void MoveUp()
     {
-        GameObject tempPanel = currentPanel;
-
-        //current panel should go up
-        currentPanel.transform.DOMove(topPanelPosition,2f,false);
-        //bottom panel should move up 
-        bottomPanel.transform.DOMove(currentPanelPosition, 2f, false);
-        //topPanel now needs ti move to bottom panel position
-        topPanel.transform.position = bottomPanelPosition;
-
-        currentPanel = bottomPanel;
-        bottomPanel = topPanel;
-        topPanel = tempPanel;
+       
     }
 
     private void MoveDown()
     {
-        GameObject tempPanel = currentPanel;
-
-        //current panel must go down
-        currentPanel.transform.DOMove(bottomPanelPosition, 2f, false);
-        //top five panel must come down
-        topPanel.transform.DOMove(currentPanelPosition, 2f, false);
-        //bottomPanel now needs to move to top panel position
-        bottomPanel.transform.position = topPanelPosition;
-
-        currentPanel = topPanel;
-        topPanel = bottomPanel;
-        bottomPanel = tempPanel;
-
+      
     }
 
 
---
+++ Assets/panelView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class panelView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI displayName;
+    [SerializeField] private TextMeshProUGUI serialNumber;
+    [SerializeField] private TextMeshProUGUI highScore;
+
+    public void setPanelValue(string sNo,string name,string score)
+    {
+        displayName.text = name;
+        serialNumber.text = sNo;
+        highScore.text = score;
+    }
+
+}
